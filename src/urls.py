4a93d1e74e4ea85f15213d66d3589bfa2b33da80
maratahmetov <ax-marat@mail.ru>
2023-05-06 15:46:00 +0300

    Oauth

---
+++ src/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+from src import views
+
+urlpatterns = [
+    path('accounts/', include('allauth.urls')),
+    path('', views.main),
+    path('profile/', views.some_view),
+]
---
+++ src/views.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+from allauth.socialaccount.models import SocialAccount
+
+
+def some_view(request):
+    # Получение объекта SocialAccount текущего пользователя
+    social_account = SocialAccount.objects.get(user=request.user, provider='google')
+    # Получение информации о пользователе
+    user_info = {
+        'name': social_account.extra_data['name'],
+        'email': social_account.extra_data['email'],
+        'first_name': social_account.extra_data['given_name'],
+        'last_name': social_account.extra_data['family_name'],
+        'avatar_url': social_account.extra_data['picture'],
+    }
+    return render(request, 'src/profile.html', {'user_info': user_info})
+
+
+def main(request):
+    return render(request, 'src/Googlelink.html')
